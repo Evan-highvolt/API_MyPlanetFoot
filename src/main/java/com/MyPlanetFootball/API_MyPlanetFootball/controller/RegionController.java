89f3006ee4b5d29664278b93087e5ad7dbdd5795
Evan-highvilt <florent.vaidie3@etu.univ-lorraine.fr>
2025-04-15 15:33:29 +0200

    Feat: Finalization de region service et controlle

--- src/main/java/com/MyPlanetFootball/API_MyPlanetFootball/controller/RegionController.java
+++ src/main/java/com/MyPlanetFootball/API_MyPlanetFootball/controller/RegionController.java
@@ -7,13 +7,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
+/**
+ * The type Region controller.
+ */
 @Slf4j
 @RestController
 @RequestMapping("/region")
@@ -21,6 +21,12 @@ public class RegionController {
     @Autowired
     private RegionService regionService;
 
+
+    /**
+     * Gets all regions.
+     *
+     * @return the all regions
+     */
     @GetMapping("/regions")
     public Iterable<RegionModel> getAllRegions() {
         try {
@@ -31,6 +37,12 @@ public class RegionController {
         }
     }
 
+    /**
+     * Gets region by id.
+     *
+     * @param id the id
+     * @return the region by id
+     */
     @GetMapping("/{id}")
     public ResponseEntity<?> getRegionById(@PathVariable @Valid Integer id) {
         Optional<RegionModel> regionId = regionService.getRegionById(id);
@@ -39,6 +51,52 @@ public class RegionController {
         } else {
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
                     .body("Région non trouvée avec l'id : " + id);
+        }
+    }
+
+    /**
+     * Create region response entity.
+     *
+     * @param region the region
+     * @return the response entity
+     */
+    @PostMapping("/addregion")
+    public ResponseEntity<String> createRegion(@RequestBody @Valid RegionModel region){
+        try {
+            regionService.createRegion(region);
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body("Région créée avec succès : " + region.getNomReg());
+        } catch (RuntimeException rte) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Erreur lors de la création de la région : " + rte.getMessage());
+        }
+    }
+
+    @PutMapping("/updateregion/{id}")
+    public ResponseEntity<?> updateRegion(@PathVariable @Valid Integer id, @RequestBody RegionModel region){
+        try {
+            RegionModel regionUpdated = regionService.updateRegion(id, region);
+            return ResponseEntity.ok(regionUpdated);
+        } catch (RuntimeException rte) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(rte.getMessage());
+        }
+    }
+
+    /**
+     * Delete region response entity.
+     *
+     * @param id the id
+     * @return the response entity
+     */
+    @DeleteMapping("/deleteregion/{id}")
+    public ResponseEntity<String> deleteRegion(@PathVariable @Valid Integer id) {
+        try {
+            regionService.deleteRegionById(id);
+            return ResponseEntity.ok("Région supprimée avec succès !");
+        } catch (RuntimeException rte) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Échec de la suppression : " + rte.getMessage());
         }
     }
 
