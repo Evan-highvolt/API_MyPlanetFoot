c472f4e3ba875912d2eaa26af27ae435dcbb7cbe
Evan-highvilt <florent.vaidie3@etu.univ-lorraine.fr>
2025-04-29 14:34:46 +0200

    Feat: Controller er service de pub

---
+++ src/main/java/com/MyPlanetFootball/API_MyPlanetFootball/controller/PublicationController.java
@@ -0,0 +1,58 @@
+package com.MyPlanetFootball.API_MyPlanetFootball.controller;
+
+import com.MyPlanetFootball.API_MyPlanetFootball.model.CompteModel;
+import com.MyPlanetFootball.API_MyPlanetFootball.model.PublicationModel;
+import com.MyPlanetFootball.API_MyPlanetFootball.service.CompteService;
+import com.MyPlanetFootball.API_MyPlanetFootball.service.PublicationService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@Slf4j
+@RequestMapping("/publication")
+public class PublicationController {
+    @Autowired
+    private PublicationService pubService;
+
+    @Autowired
+    private CompteService compteService;
+
+
+    @GetMapping()
+    public ResponseEntity<?> getPublications() {
+        try {
+            Iterable<PublicationModel> publication = pubService.getpublications();
+            return ResponseEntity.ok(publication);
+        } catch (Exception e){
+            log.error("Erreur lors de la recherche des publication : {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Problem lors de la recuperation de la liste" );
+        }
+    }
+
+
+    @GetMapping("/{mail}")
+    public ResponseEntity<?> getPublicationsParCompte(@PathVariable String mail) {
+        try {
+            Optional<CompteModel> compteOpt = compteService.GetCompteByLogin(mail);
+            if (compteOpt.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                        .body("Le compte n'est pas trouvé");
+            }
+            return ResponseEntity.ok(compteOpt.get());
+        } catch (Exception e) {
+            log.error("Erreur lors de la recherche du compte : {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Problème lors de la récupération des publications");
+        }
+    }
+
+//    @DeleteMapping()
+
+}
---
+++ src/main/java/com/MyPlanetFootball/API_MyPlanetFootball/service/PublicationService.java
@@ -0,0 +1,51 @@
+package com.MyPlanetFootball.API_MyPlanetFootball.service;
+
+
+import com.MyPlanetFootball.API_MyPlanetFootball.model.CompteModel;
+import com.MyPlanetFootball.API_MyPlanetFootball.model.PublicationModel;
+import com.MyPlanetFootball.API_MyPlanetFootball.repo.CompteRepo;
+import com.MyPlanetFootball.API_MyPlanetFootball.repo.PublicationRepo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+@Slf4j
+public class PublicationService {
+    @Autowired
+    private PublicationRepo pubRepo;
+
+    @Autowired
+    private CompteRepo compteRepo;
+
+
+    public Iterable<PublicationModel>getpublications() {
+        try {
+
+            return pubRepo.findAll();
+        } catch (Exception e) {
+            log.error("Erreur lors de la mise à jour de la recherche des publication pour le compte", e);
+            throw new RuntimeException("Erreur lors de la récupération des publications : " + e.getMessage(), e);
+        }
+    }
+
+    public List<PublicationModel> getPubByCompte(String emailCpt) {
+        try {
+           Optional<CompteModel> pubCpt = compteRepo.findByLoginCpt(emailCpt);
+           if (pubCpt.isEmpty()) {
+               throw new RuntimeException("Aucun compte trouvé avec l'email : " + emailCpt);
+           }
+            return pubRepo.getPublicationModelByCompteModel_LoginCpt(emailCpt);
+        } catch (Exception e) {
+            log.error("Problème lors de la récupération des publications du compte : {}", emailCpt, e);
+            throw new RuntimeException("Impossible de récupérer les publications du compte : " + emailCpt);
+        }
+
+    }
+
+
+}
